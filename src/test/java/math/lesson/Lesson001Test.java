f6646c073fae57cc66566e44698b45e75c34f491
Hibernate Pano <362472407@qq.com>
2020-05-23 00:29:44 +0800

    todo 二进制转换未曾考虑十进制数为负数的情况

--- src/test/java/math/lesson/Lesson001Test.java
+++ src/test/java/math/lesson/Lesson001Test.java
@@ -7,16 +7,24 @@ import static org.testng.Assert.*;
 public class Lesson001Test {
 
     @Test
-    void decimalToBinary() {
+    void decimalToBinary01() {
         int decimal = 101;
 
         Lesson001 lesson001 = new Lesson001();
         String binary01 = lesson001.decimalToBinary01(decimal);
-        String binary02 = lesson001.decimalToBinary02(decimal);
-
         System.out.println("binary01 = " + binary01);
+
+        assertEquals(binary01, Integer.toBinaryString(decimal));
+    }
+
+    @Test
+    void decimalToBinary02() {
+        int decimal = 101;
+
+        Lesson001 lesson001 = new Lesson001();
+        String binary02 = lesson001.decimalToBinary02(decimal);
         System.out.println("binary02 = " + binary02);
 
-        assertEquals(binary01, binary02);
+        assertEquals(binary02, Integer.toBinaryString(decimal));
     }
 }
